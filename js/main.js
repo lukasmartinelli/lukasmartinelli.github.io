60fe69d97ae9e5c8adde7eca3c650743d0d5b17c
Lukas Martinelli <me@lukasmartinelli.ch>
2013-09-04 21:31:51 +0200

    Fixed issue with main.js and font being loaded

--- js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-	var sidebar_visible = true;
-
-	document.getElementById('nav-control-toggle').onclick = function(event){ 		
-		sidebar_visible = !sidebar_visible;
-		document.getElementById('sidebar').style.display = sidebar_visible ? 'block' : 'none';
-		event.preventDefault();
-	};
-};
