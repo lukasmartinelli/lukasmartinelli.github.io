e225e6b538a09eb7059d20179824bc58c127a0aa
Lukas Martinelli <me@lukasmartinelli.ch>
2014-07-21 10:59:38 +0200

    Made CSS Tables beautiful

--- js/responsive-table.js
+++ js/responsive-table.js
@@ -1,16 +1,23 @@
 (function(){
-    var headertext = [],
-    headers = document.querySelectorAll("table th"),
-    tablerows = document.querySelectorAll("table th"),
-    tablebody = document.querySelector("table tbody");
+    var tables = document.querySelectorAll("table");
+    var addDataAttributes = function(table) {
+        var headertext = [],
+        headers = table.querySelectorAll("table th"),
+        tablerows = table.querySelectorAll("table th"),
+        tablebody = table.querySelector("table tbody");
 
-    for(var i = 0; i < headers.length; i++) {
-        var current = headers[i];
-        headertext.push(current.textContent.replace(/\r?\n|\r/,""));
-
-    for (var i = 0, row; row = tablebody.rows[i]; i++) {
-        for (var j = 0, col; col = row.cells[j]; j++) {
-            col.setAttribute("data-th", headertext[j]);
+        for(var i = 0; i < headers.length; i++) {
+            var current = headers[i];
+            headertext.push(current.textContent.replace(/\r?\n|\r/,""));
         }
+        for (var i = 0, row; row = tablebody.rows[i]; i++) {
+            for (var j = 0, col; col = row.cells[j]; j++) {
+                col.setAttribute("data-th", headertext[j]);
+            }
+        }
+    };
+
+    for (var i = 0; i < tables.length; i++) {
+        addDataAttributes(tables[i]);
     }
 })();
